e4ef323a2f21b9620083833f480a946e6ace34b8
WAIK <WAIK@SITE.CDI.PRIVATE>
2023-01-08 01:25:43 -0600

    payment form

--- resources/views/pages/checkout_payment.blade.php
+++ resources/views/pages/checkout_payment.blade.php
@@ -6,6 +6,11 @@
 		</div>
 	</div>
 
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('public/assests/payment_form/css/styles.css')}}">
+    <link rel="stylesheet" type="text/css" href="asset('public/assests/payment_form/css/demo.css')}}">
+
 	@if (Session::has('cart_message'))
 		<div class="alert alert-info">{{ Session::get('cart_message') }}</div>
 		@php Session::forget('cart_message'); @endphp
@@ -17,31 +22,72 @@
 	</style>
 	<div class="carrito">
 		<div class="container">
-			<div class="row hide-mb">
-				<div class="col-md-12">
-					<h2 class="title">Carrito</h2>
-				</div>
-			</div>
 			<div class="crt-rep">
 					<div class="row align-items-center">
-						<div class="">
-							<div class="prd-crt">
-								<div class="d-flex flex-wrap prd-inner">
-									 <form action="{{url('online_payment')}}" method="get" class="paymentWidgets" data-brands="VISA MASTER AMEX">
-									 	@csrf
-									 </form>
-								</div> 
-							</div> 
-						</div> 
+						    <div class="container-fluid">
+        <div class="creditCardForm">
+            <div class="heading">
+                <h1>Confirm Purchase</h1>
+            </div>
+            <div class="payment" name="payment_frm" method="post" action="{{url('online_payment')}}">
+                <form>
+                	@csrf
+                    <div class="form-group owner">
+                        <label for="owner">Owner</label>
+                        <input type="text" class="form-control" id="owner">
+                    </div>
+                    <div class="form-group CVV">
+                        <label for="cvv">CVV</label>
+                        <input type="text" class="form-control" id="cvv">
+                    </div>
+                    <div class="form-group" id="card-number-field">
+                        <label for="cardNumber">Card Number</label>
+                        <input type="text" class="form-control" id="cardNumber">
+                    </div>
+                    <div class="form-group" id="expiration-date">
+                        <label>Expiration Date</label>
+                        <select>
+                            <option value="01">January</option>
+                            <option value="02">February </option>
+                            <option value="03">March</option>
+                            <option value="04">April</option>
+                            <option value="05">May</option>
+                            <option value="06">June</option>
+                            <option value="07">July</option>
+                            <option value="08">August</option>
+                            <option value="09">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
+                        <select>
+                            <option value="16"> 2016</option>
+                            <option value="17"> 2017</option>
+                            <option value="18"> 2018</option>
+                            <option value="19"> 2019</option>
+                            <option value="20"> 2020</option>
+                            <option value="21"> 2021</option>
+                        </select>
+                    </div>
+                    <div class="form-group" id="credit_cards">
+                        <img src="{{asset('public/assests/payment_form/images/visa.jpg')}}" id="visa">
+                        <img src="{{asset('public/assests/payment_form/images/mastercard.jpg')}}" id="mastercard">
+                        <img src="{{asset('public/assests/payment_form/images/amex.jpg')}}" id="amex">
+                    </div>
+                    <div class="form-group" id="pay-now">
+                        <button type="submit" class="btn btn-default" id="confirm-purchase">Confirm</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div> 
 					</div>
 			</div> 
 		</div> 
 	</div> 
 
-	<script src="https://code.jquery.com/jquery.js" type="text/javascript"></script>
-	<script type="text/javascript">
-		setTimeout(() => $('.wpwl-form:first').remove(),1000)
-	  	var wpwlOptions = {    registrations: {        requireCvv: false,        hideInitialPaymentForms: false    }}
-    </script>
-    <script async src="https://eu-test.oppwa.com/v1/paymentWidgets.js?checkoutId=6847A2F24217BC57D82013B335EF2310.uat01-vm-tx01"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="{{asset('public/assests/payment_form/js/jquery.payform.min.js')}}" charset="utf-8"></script>
+    <script src="{{asset('public/assests/payment_form/js/script.js')}}"></script>
 @stop
